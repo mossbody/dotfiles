1811f8c7d06eb5c49625eb8ac00cdee9e5f38de1
Jorge Rubio <228247493+mossbody@users.noreply.github.com>
2025-08-24 06:55:00 -0600

    Add initial configuration files for Qtile
    - Created autostart script for Qtile.
    - Added Gruvbox color scheme configuration.
    - Set up main configuration file for Qtile.
    - Included a sample wallpaper (astro.jpg).

---
+++ .config/qtile/colors.py
@@ -0,0 +1,13 @@
+Gruvbox = {
+    "dark": ["#141819"],     # Fondo oscuro
+    "light": ["#ebdbb2"],    # Color base claro
+    "gray": ["#a89984"],     # Gris neutro
+    "red": "#cc241d",        # Rojo
+    "green": "#98971a",      # Verde
+    "yellow": "#d79921",     # Amarillo
+    "blue": "#458588",       # Azul
+    "magenta": "#b16286",    # Magenta
+    "cyan": "#689d6a",       # Cyan
+    "white": "#fbf1c7",      # Blanco cálido
+    "orange": "#d65d0e",     # Naranja
+}
